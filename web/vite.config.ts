a3edaacded0d4e79291fd687f8c5b0b77410ed42
jhurliman <jhurliman@jhurliman.org>
2025-08-10 23:40:50 -0700

    feat: deploy to github pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Base must match the repository name for GitHub Pages when using <user>.github.io/<repo>
+  // If deploying to a custom domain or user/organization page, adjust/remove accordingly.
+  base: '/take5bot/',
   plugins: [
     react(),
     tailwindcss(),
